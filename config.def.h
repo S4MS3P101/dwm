255d46c4fb5059a256cff2c451535ca85f5d5d82
samsepi01 <problem.dog@protonmail.com>
2022-12-31 19:37:52 +0530

    updated colors, gaps & tags

--- config.def.h
+++ config.def.h
@@ -2,16 +2,16 @@
 
 /* appearance */
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
-static const unsigned int gappx     = 7;        /* gaps between windows */
+static const unsigned int gappx     = 9;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "JetBrains Mono:size=9" };
-static const char dmenufont[]       = "JetBrains Mono:size=9";
+static const char *fonts[]          = { "JetBrains Mono:size=8"};
+static const char dmenufont[]       = "JetBrains Mono:size=8";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#444444";
+static const char col_gray4[]       = "#282828";
 static const char col_cyan[]        = "#a89984";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
@@ -20,7 +20,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "1", "2", "3", "4", "5" };
 
 static const Rule rules[] = {
 	/* xprop(1):
